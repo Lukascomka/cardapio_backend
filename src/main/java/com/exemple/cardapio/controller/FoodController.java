1ead59b88eb38f9be2855a8b661328f1b1722b6b
lukascomka <lukas.souza@sptech.school>
2025-09-04 18:40:43 -0300

    Projeto backend

---
+++ src/main/java/com/exemple/cardapio/controller/FoodController.java
@@ -0,0 +1,38 @@
+package com.exemple.cardapio.controller;
+
+import com.exemple.cardapio.food.Food;
+import com.exemple.cardapio.food.FoodRepository;
+import com.exemple.cardapio.food.FoodRequestDTO;
+import com.exemple.cardapio.food.FoodResponseDTO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.security.PublicKey;
+import java.util.List;
+
+@RestController
+@RequestMapping("food")
+public class FoodController {
+
+    @Autowired
+    private FoodRepository repository;
+
+    @CrossOrigin(origins ="*", allowedHeaders = "*")
+    @PostMapping
+    public void saveFood(@RequestBody FoodRequestDTO data){
+
+        Food foodData = new Food(data);
+        repository.save(foodData);
+        return;
+    }
+
+    @CrossOrigin(origins = "", allowedHeaders = "*")
+    @GetMapping
+    public List<FoodResponseDTO> getAll(){
+
+
+    List<FoodResponseDTO> foodList = repository.findAll().stream().map(FoodResponseDTO::new).toList();
+    return foodList;
+
+    }
+}
---
+++ src/main/java/com/exemple/cardapio/food/FoodRequestDTO.java
@@ -0,0 +1,6 @@
+package com.exemple.cardapio.food;
+
+public record FoodRequestDTO(String title, String imge, Integer price) {
+
+
+}
